b2095e27dc8ce4a8804f8412162697945a5fb284
DESKTOP-KE0NGQ1\Sasha <Sasha7b9@tut.by>
2021-11-05 14:25:23 +0300

    *

---
+++ Demo/scripts/win/install/urho/assembly.bat
@@ -0,0 +1,56 @@
+@echo off
+
+if "%1" equ "" goto HINT
+if "%2" equ "" goto HINT
+if "%3" neq "" goto HINT
+
+set isMake=0
+set isBuild=0
+set isDebug=0
+set isRelease=0
+
+set current_dir=%CD%
+
+cd ../../../..
+
+if not exist Urho3D git clone https://github.com/Sasha7b9/Urho3D.git& set isMake = 1
+
+cd Urho3D
+
+git pull
+
+:START_WITH_MAKE
+if %1==build set isBuild=1 & goto LABEL1
+if %1==full  set isMake=1 & set isBuild=1 & goto LABEL1
+
+goto HINT
+
+:LABEL1
+if %2==debug   set isDebug=1 & goto LABEL2
+if %2==release set isRelease=1 & goto LABEL2
+if %2==all     set isDebug=1 & set isRelease=1 & goto LABEL2
+
+goto HINT
+
+:LABEL2
+if %isMake% equ 0 goto BUILD
+
+:MAKE
+if %isDebug%   equ 1 call %current_dir%\make.bat debug
+if %isRelease% equ 1 call %current_dir%\make.bat release
+
+:BUILD
+if %isBuild%   equ 0 goto EXIT
+if %isDebug%   equ 1 call %current_dir%\build.bat debug
+if %isRelease% equ 1 call %current_dir%\build.bat release
+goto EXIT
+
+:HINT
+echo.
+echo Using assembly.bat:
+echo                    assembly.bat [make^|build^|full] [debug^|release^|all]
+echo.
+goto EXIT
+
+:EXIT
+cd %current_dir%
---
+++ Demo/scripts/win/install/urho/make.bat
@@ -0,0 +1,36 @@
+@echo off
+
+if "%1" equ "" goto HINT
+if "%2" neq "" goto HINT
+
+set BUILD_TYPE=1
+set RUNTIME_TYPE=0
+
+if %1==debug set BUILD_TYPE=Debug& goto MAKE
+if %1==release set BUILD_TYPE=Release& set RUNTIME_TYPE=1 & goto MAKE
+
+goto HINT
+
+:STATIC_RUNTIME
+if %2==0 goto MAKE
+if %2==1 goto MAKE
+
+goto HINT
+
+:MAKE
+cd ../../../..
+rmdir generated\U-Urho\%1 /S /Q
+cd ../src/U-Urho
+cmake . -B..\..\Demo\generated\U-Urho\%1 -DCMAKE_GENERATOR_PLATFORM=x64 -DCMAKE_GENERATOR="Visual Studio 16 2019" -DCMAKE_BUILD_TYPE=%BUILD_TYPE% -DURHO3D_64BIT=1 -DURHO3D_THREADING=1 -DURHO3D_LUA=1 -DURHO3D_ANGELSCRIPT=1 -DURHO3D_LIB_TYPE=SHARED -DURHO3D_STATIC_RUNTIME=%RUNTIME_TYPE%
+cd ../../Demo/scripts/win/install/urho
+goto EXIT
+
+:HINT
+
+echo.
+echo Using make.bat:
+echo                make.bat [debug^|release]
+echo.
+goto EXIT
+
+:EXIT
